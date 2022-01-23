2ed99ebddfbbce399bb5d54ef4cbbec90138c656
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-01-23 18:28:13 -0300

    feat: post/tweets, user tweet

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ server.use(cors())
 server.use(express.json())
 
 const users = []
+const tweets = []
 
 server.post('/sign-up',(req, res)=>{
     const user = req.body;
     avatarTweet = user.avatar;
     users.push(user)
+    res.send("OK")
+})
+
+server.post('/tweets',(req, res)=>{
+    const tweet = req.body;
+    tweets.push(tweet)
     res.send("OK")
 })
 
