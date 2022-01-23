94b5f74d6afcc3dce5657e3c3631d02a2f4d5e5a
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-01-23 18:25:33 -0300

    feat: post/sign-up, user login

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+
+const server = express();
+server.use(cors())
+server.use(express.json())
+
+const users = []
+
+server.post('/sign-up',(req, res)=>{
+    const user = req.body;
+    avatarTweet = user.avatar;
+    users.push(user)
+    res.send("OK")
+})
+
+server.listen(5000,() => {
+    console.log("API ATIVA")
+});
